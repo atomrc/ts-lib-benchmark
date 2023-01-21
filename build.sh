fe22059ce622333bde82e8c810c04b794fb1d938
Thomas Belin <me@thomasbelin.fr>
2023-01-21 14:41:03 +0100

    build script

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 rm -fr dist 
-npx esbuild src/*.ts --target=es2017 --tree-shaking=true --outdir=./dist/esbuild
-npx tsup src/*.ts --format esm --target=es2017 --treeshake -d=./dist/tsup
-npx swc -d ./dist/swc -C jsc.target=es2017 src/*.ts
-npx tsc --outDir ./dist/tsc
+echo "\nesbuild"
+time npx esbuild src/*.ts --target=es2017 --tree-shaking=true --outdir=./dist/esbuild 2> /dev/null
+
+echo "\ntsup"
+time npx tsup src/*.ts --format esm --target=es2017 --treeshake -d=./dist/tsup > /dev/null
+
+echo "\nswc"
+time npx swc -d ./dist/swc -C jsc.target=es2017 src/*.ts > /dev/null
+
+echo "\ntsc"
+time npx tsc --outDir ./dist/tsc 2> /dev/null
