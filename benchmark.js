7c885699a90ee2d656984a4ada47954f7ab0fa04
Thomas Belin <me@thomasbelin.fr>
2023-01-21 19:46:44 +0100

    chore: Improve printing benchmark results

--- benchmark.js
+++ benchmark.js
@@ -2,28 +2,45 @@ import { execSync } from "child_process";
 
 const commands = {
   esbuild: `
-    npx esbuild src/*.ts --format=esm --outdir=./lib/esbuild/esm 2> /dev/null
-    npx esbuild src/*.ts --format=cjs --outdir=./lib/esbuild/cjs 2> /dev/null
-    npx tsc --emitDeclarationOnly --outDir ./lib/esbuild/types`,
-
-  swc: `
-    npx swc -C module.type=es6 src/*.ts -d ./lib/swc/esm 
-    npx swc -C module.type=commonjs src/*.ts -d ./lib/swc/cjs 
-    npx tsc --emitDeclarationOnly --outDir ./lib/swc/types
+    npx esbuild src/*.ts --format=esm --outdir=./lib/esbuild/esm 2> /dev/null &
+    npx esbuild src/*.ts --format=cjs --outdir=./lib/esbuild/cjs 2> /dev/null &
     `,
 
-  tsup: `npx tsup src/*.ts --format esm,cjs --dts -d=./lib/tsup`,
+  swc: `
+    npx swc -C module.type=es6 src/*.ts -d ./lib/swc/esm &
+    npx swc -C module.type=commonjs src/*.ts -d ./lib/swc/cjs &
+    `,
+
+  tsup: `npx tsup src/*.ts --format esm,cjs -d=./lib/tsup`,
   tsc: `
-    npx tsc --module esnext --outDir ./lib/tsc/esm
-    npx tsc --module commonjs --outDir ./lib/tsc/cjs
+    npx tsc --module esnext --declaration false --outDir ./lib/tsc/esm &
+    npx tsc --module commonjs --declaration false --outDir ./lib/tsc/cjs
 `,
 };
 
-execSync("rm -fr lib");
+function benchmark(command, onResult) {
+  const results = [];
+  for (let i = 0; i < 10; i++) {
+    const timeStart = Date.now();
+    execSync(command);
+    const totalTime = Date.now() - timeStart;
+    results.push(totalTime);
+    onResult(totalTime);
+  }
+  return results;
+}
+
+const results = {};
 for (const [tool, command] of Object.entries(commands)) {
-  console.log(`benchmarking ${tool}`);
-  const timeStart = Date.now();
-  execSync(command);
-  const totalTime = Date.now() - timeStart;
-  console.log(`took ${totalTime}ms`);
+  execSync("rm -fr lib");
+  console.warn(`benchmarking ${tool}`);
+  const result = benchmark(command, console.warn);
+  results[tool] = result;
+}
+
+for (let [tool, sample] of Object.entries(results)) {
+  const max = Math.max(...sample);
+  const min = Math.min(...sample);
+  const mean = sample.reduce((acc, val) => acc + val) / sample.length;
+  console.log(`${tool}: min: ${min}, mean: ${mean}, max: ${max}`);
 }
