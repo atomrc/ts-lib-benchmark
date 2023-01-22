8cc8879fd4f4f0eebafadea1034048564e7f9da4
Thomas Belin <me@thomasbelin.fr>
2023-01-22 19:33:49 +0100

    chore: restore declaration file generation

--- benchmark.js
+++ benchmark.js
@@ -4,17 +4,20 @@ const commands = {
   esbuild: `
     npx esbuild src/*.ts --format=esm --outdir=./lib/esbuild/esm 2> /dev/null &
     npx esbuild src/*.ts --format=cjs --outdir=./lib/esbuild/cjs 2> /dev/null &
+    npx tsc --emitDeclarationOnly --outDir ./lib/esbuild/types
     `,
 
   swc: `
     npx swc -C module.type=es6 src/*.ts -d ./lib/swc/esm &
     npx swc -C module.type=commonjs src/*.ts -d ./lib/swc/cjs &
+    npx tsc --emitDeclarationOnly --outDir ./lib/scw/types
     `,
 
-  tsup: `npx tsup src/*.ts --format esm,cjs -d=./lib/tsup`,
+  tsup: `npx tsup src/*.ts --format esm,cjs --dts -d=./lib/tsup`,
+
   tsc: `
-    npx tsc --module esnext --declaration false --outDir ./lib/tsc/esm &
-    npx tsc --module commonjs --declaration false --outDir ./lib/tsc/cjs
+    npx tsc --module esnext --outDir ./lib/tsc/esm &
+    npx tsc --module commonjs --outDir ./lib/tsc/cjs
 `,
 };
 
